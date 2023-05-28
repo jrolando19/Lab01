48470513256008a65826d84aaa4b81a8eadbc8c9
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 18:19:25 -0500

    Se prueba ejemplo de recursión entre 2 números

--- Recursion.java
+++ Recursion.java
@@ -1,15 +1,14 @@
 public class Recursion {
     public static void main(String[] args) {
-        int result = sum(10);
+        int result = sum(5, 10);
         System.out.println(result);
     }
 
-    public static int sum(int k) {
-        if (k > 0) { // Evalua si el número es mayor que 0
-            return k + sum(k - 1); // Hace el cálculo luego de verificar y lo suma al mismo menos 1 hasta que se
-                                   // rompa la verificación
+    public static int sum(int start, int end) {// Usa la recursividad para sumar todos los números entre 5 y 10
+        if (end > start) {// Verificar si el número es mayor al otro
+            return end + sum(start, end - 1);// Se hace la suma n veces hasta romper la verificación
         } else {
-            return 0;
+            return end;
         }
     }
 }
