baf7619e31292435bf2dfc114ed9f07a7d400d30
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 18:10:27 -0500

    Se muestra el alcance del método usando bloques de código con {}

--- Scope.java
+++ Scope.java
@@ -2,13 +2,16 @@ public class Scope {
     public static void main(String[] args) {
 
         // El código aquí no puede usar x
+        {
+            int x = 100;
 
-        int x = 100;
+            // El código aquí puede usar x
+            System.out.println(x);
+        }
+        // El código aquí no puede usar x
 
-        // El código aquí puede usar x
-        System.out.println(x);
-        // Las variables declaradas directamente dentro de un método están disponibles
-        // en cualquier parte del método siguiendo la línea de código en la que fueron
-        // declaradas:
+        // Solo se puede acceder a las variables declaradas dentro de bloques de código
+        // mediante el código entre llaves, que sigue la línea en la que se declaró la
+        // variable
     }
 }
