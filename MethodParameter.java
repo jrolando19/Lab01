13336239f6a5ac69625cd188bd33073728a6a7a5
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 16:25:52 -0500

    MethodParameter-Se añade parámetro al método para ver su comportamiento

---
+++ MethodParameter.java
@@ -0,0 +1,11 @@
+public class MethodParameter {
+    public static void main(String[] args) {
+        myMethod("Liam");
+        myMethod("Jenny");
+        myMethod("Anja");
+    }
+
+    public static void myMethod(String fname) {
+        System.out.println(fname + " Refsnes");
+    }
+}
