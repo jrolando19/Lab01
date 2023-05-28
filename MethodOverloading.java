4d60dc81f5c173184061630baffe6d9338890e02
Jordy Tejada <jtejadala@unsa.edu.pe>
2023-05-28 17:54:59 -0500

    Comprueba la sobrecarga de métodos de diferentes tipos de datos pero mismo nombre de método

--- MethodOverloading.java
+++ MethodOverloading.java
@@ -1,17 +1,18 @@
 public class MethodOverloading {
     public static void main(String[] args) {
-        int myNum1 = plusMethodInt(8, 5); // Se hace llamado al método de tipo int
-        double myNum2 = plusMethodDouble(4.3, 6.26); // Se hace llamado al método de tipo double
+        int myNum1 = plusMethod(8, 5); // Se hace llamado al método de tipo int
+        double myNum2 = plusMethod(4.3, 6.26); // Se hace llamado al método de tipo double
         System.out.println("int: " + myNum1);
         System.out.println("double: " + myNum2);
-        // Se muestra 2 métodos que suman números de diferentes tipos
+        // Se comprueba la sobrecarga de métodos al ser del mismo nombre pero que sus
+        // tipos de parámetros son diferentes
     }
 
-    private static double plusMethodDouble(double x, double y) {
+    private static double plusMethod(double x, double y) {
         return x + y;// retorna la suma de los valores de tipo double
     }
 
-    private static int plusMethodInt(int x, int y) {
+    private static int plusMethod(int x, int y) {
         return x + y;// retorna la suma de los valores de tipo int
     }
 }
