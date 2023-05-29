a48e158406005e9bdcd812570f34ce970aa8baed
gustavoyunque <gyunque@unsa.edu.pe>
2023-05-28 21:37:51 -0500

    impresion de un arreglo

---
+++ ejercicio15.java
@@ -0,0 +1,21 @@
+
+public class ejercicio15 {
+
+	public static void main(String[] args) {
+		
+		
+
+try
+ {
+  int[] myNumbers = {1, 2, 3};
+  System.out.println(myNumbers[10]);
+} 
+catch
+ (Exception e) {
+  System.out.println("Something went wrong.");
+}
+		
+		
+	}
+
+}
