7c7cac4ba094fe104098683f81741de1ccd69182
gustavoyunque <gyunque@unsa.edu.pe>
2023-05-28 21:34:02 -0500

    aplicacion del bucle while

---
+++ ejercicio10.java
@@ -0,0 +1,18 @@
+
+public class ejercicio10 {
+
+	
+	// mientras y sea menor que 6 se imprimira //
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while
+		 (i < 6) {
+		  System.out.println(i);
+		  
+		i++
+		;
+		}
+		
+	}
+}
