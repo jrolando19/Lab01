321ebbf35871761a6195960eb556c6a899c02ff1
gustavoyunque <gyunque@unsa.edu.pe>
2023-05-28 21:33:23 -0500

    aplicacion de case

---
+++ ejercicio9.java
@@ -0,0 +1,25 @@
+
+public class ejercicio9 {
+
+	
+	// aplicacion sencilla de case //
+	
+	public static void main(String[] args) {
+		
+		int day = 2;
+		switch (day) 
+		{
+		  
+		case 1:
+		    System.out.println("Saturday");
+		    break;
+		  
+		case  2:
+		    System.out.println("Sunday");
+		    break;
+		
+		}
+		
+		
+	}
+}
